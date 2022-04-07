0fc07316697ecee6aae750e99cef8c03fd1a5dca
carlosriobos <riobosantomecarlos@gmail.com>
2022-04-07 13:03:55 +0200

    adding new method getAllCharactersFromAPI

--- src/main/java/com/example/demo/services/RickNMortyService.java
+++ src/main/java/com/example/demo/services/RickNMortyService.java
@@ -23,9 +23,21 @@ public class RickNMortyService {
     }
 
     public ArrayList<Person> getCharactersFromAPI() {
-        String url = "https://rickandmortyapi.com/api/character/";
+        String url = "https://rickandmortyapi.com/api/character";
         Persons persons = restTemplate.getForObject(url, Persons.class);
         return persons.results;
     }
+/*  //Burrada porque se sobrecarga la ram del procesador del cliente
+
+    public ArrayList<Person> getAllCharactersFromAPI() {
+        ArrayList<Person> allPersons = new ArrayList<Person>();
+        for(int i=1; i<=42; i++) {
+            String url = "https://rickandmortyapi.com/api/character?page="+i;
+            Persons persons = restTemplate.getForObject(url, Persons.class);
+            allPersons.addAll(persons.results);
+        }
+        return allPersons;
+    }
+*/
 }
 
